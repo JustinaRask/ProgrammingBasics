bf7c8c8e8d343734275c5ee863be2b8a4838d976
Justina Raškauskienė <raskajus@siriusit.com>
2016-06-26 21:27:29 +0300

    Competence levels in array

--- 1 namu darbas/1 namu darbas/Program.cs
+++ 1 namu darbas/1 namu darbas/Program.cs
@@ -10,38 +10,57 @@ namespace _1_namu_darbas
     {
         static void Main(string[] args)
         {
+            string[] competenceLevelArray = new string[] { "Entry", "Junior", "Mid-Level", "Professional", "Senior" };
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Employee experience in years?");
+                string input = Console.ReadLine();
+                double workExperienceInYears;
 
-            Console.WriteLine("kokia yra darbuotojo darbo patirtis?");
-         
-            string input = Console.ReadLine();
-            double d;
-            Double.TryParse(input, out d);
+                if (!double.TryParse(input, out workExperienceInYears))
+                {
+                    if (input == "q")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Klaida: Ivestas blogas argumentas. Ivesti galima tik skaiciu");
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (workExperienceInYears < 0 || workExperienceInYears > 100)
+                    {
+                        Console.WriteLine("Klaida: Darbo patirtis metais negali buti mazesne nei 0 arba didesne nei 100");
+                        continue;
+                    }
+                }
 
-            if (d == 0)
-            {
-                Console.WriteLine("Entry");
-            }
-            if (d > 0 && d < 2)
-            {
-                Console.WriteLine("Junior");
-            }
-            if (d >=2 && d < 4)
-            {
-                Console.WriteLine("Mid-Level");
-            }
-            if (d >=4 &&  d < 6.5)
-            {
-                Console.WriteLine("Professional");
-            }
-            if( d >=6.5)
-            {
-                Console.WriteLine("Senior");
+                if (workExperienceInYears == 0)
+                {
+                    Console.WriteLine(competenceLevelArray[0]);
+                }
+                else if (workExperienceInYears > 0 && workExperienceInYears < 2)
+                {
+                    Console.WriteLine(competenceLevelArray[1]);
+                }
+                else if (workExperienceInYears >= 2 && workExperienceInYears < 4)
+                {
+                    Console.WriteLine(competenceLevelArray[2]);
+                }
+                else if (workExperienceInYears >= 4 && workExperienceInYears < 6.5)
+                {
+                    Console.WriteLine(competenceLevelArray[3]);
+                }
+                else if (workExperienceInYears >= 6.5 && workExperienceInYears < 100)
+                {
+                    Console.WriteLine(competenceLevelArray[4]);
+                }
+
             }
             Console.ReadLine();
-
-
-
         }
-
     }
 }
